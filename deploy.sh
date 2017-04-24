82a97bc76888ad99ab81b24c324dd037424ed577
francisco.porcel <francisco.porcel@unit4.com>
2017-04-24 10:13:12 +0200

    Added a new deploy config

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+# 3. Install NPM packages
+if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then  
+  cd "$DEPLOYMENT_TARGET"
+  eval $NPM_CMD install --production
+  eval $NPM_CMD install --only=dev
+  exitWithMessageOnError "npm failed"
+  cd - > /dev/null
+fi 
+
+# 4. Angular Prod Build
+if [ -e "$DEPLOYMENT_TARGET/.angular-cli.json" ]; then  
+  cd "$DEPLOYMENT_TARGET"
+  eval ./node_modules/.bin/ng build --prod
+  exitWithMessageOnError "Angular build failed"
+  cd - > /dev/null
+fi  
